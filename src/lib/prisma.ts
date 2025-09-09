b3646512cb5795c3765ed4367b52538405061bbc
Yeshigeta <Rashambachew@gmail.com>
2025-09-09 12:43:36 +0300

    first3

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@/generated/prisma";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PrismaClient } = require("@prisma/client");
 
 // Avoid creating multiple PrismaClient instances in dev
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: any };
 
-export const prisma: PrismaClient =
-  globalForPrisma.prisma ?? new PrismaClient({});
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
