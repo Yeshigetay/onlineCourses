72e93a0858ea610a39d660e57f1c2cfc0ed1f8db
yeshigetay <rashambachew@gmail.com>
2025-09-09 07:13:18 -0700

    Phone number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,11 +169,12 @@ export default function Home() {
               </a>
               <p className="text-center mt-2">
             <a href="tel:+251991733134" className="text-blue-600 hover:text-blue-700 underline">
-              +251 991 733 134
+              +251 991733134
             </a>
             
           </p>
-          <p><a href="tel:+251 946916891" className="text-blue-600 hover:text-blue-700 underline">
+          <p>
+          <a href="tel:+251 946916891" className="text-blue-600 hover:text-blue-700 underline">
               +251 946916891
             </a></p>
             </div>
